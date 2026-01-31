f3e7e89a7ffdfc6ff86ae66a3878eee78c545850
DTBbuilds <dtbbuildsoffice@gmail.com>
2026-01-31 23:04:51 +0300

    fix: update sitemap URL to smartduka-eta.vercel.app

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://smartduka.co.ke',
+  siteUrl: process.env.SITE_URL || 'https://smartduka-eta.vercel.app',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   
